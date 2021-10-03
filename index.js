4361aa48b1d00625229845ba74155df4a2e6d622
vismael <xjfw.3.vip@gmail.com>
2021-10-03 17:12:58 -0300

    first working commit

--- index.js
+++ index.js
@@ -5,54 +5,55 @@ const admin = require("firebase-admin");
 
 const serviceAccount = require("E:/00_Programacion2021/Holders/Back/tokenholders-firebase-adminsdk-kinwg-ce7b193a26.json");
 
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://tokenholders-default-rtdb.firebaseio.com",
 });
 
-const database = admin.database();
+
 
 // $env:GOOGLE_APPLICATION_CREDENTIALS='E:\00_Programacion2021\Holders\Back\tokenholders-firebase-adminsdk-kinwg-ce7b193a26.json'
 
-const saveData = async (name, holders) => {
-
-      const ref = database.ref(`/${name}/`);
-      const id = ref.push().key;
-      ref.child(id).set({
-        name: "cars",
-        holders: holders,
-        date: Date.now(),
-      });
-    
-      console.log(1, 'pasaaa')
-
+const saveData = async (name, holders, databaseSDK) => {
+  const ref = databaseSDK.ref(`/${name}/`);
+  const id = ref.push().key;
+  ref.child(id).set({
+    name: "cars",
+    holders: holders,
+    date: Date.now(),
+  }, (error) => {
+    if (error) {
+      console.log('Data could not be saved.' + error);
+    } else {
+      console.log('Data saved successfully.');
+      databaseSDK.app.delete();
+    }
+  } );
 };
 
-const getHolders = async (url) => {
+const getHolders = async (url, databaseSDK) => {
   // const vgmUrl =
   //   "https://bscscan.com/token/0x1228fb5ef4c98cabd696ba1bd4819e050389d21a#balances";
   console.log("quering :", url);
-  await got(url)
-    .then( async (response) => {
-      const dom = new JSDOM(response.body);
-      const holdersText = dom.window.document.querySelector(
-        "#ContentPlaceHolder1_tr_tokenHolders .mr-3"
-      ).textContent;
-      const holders = parseInt(
-        holdersText.replace(" addresses", "").replace(/,/g, ""),
-        10
-      );
+  try {
+    const response = await got(url);
 
-      console.log("Holders: ", holders);
-      await saveData('cars', holders)
-      console.log(2, 'pasaaa')
+    const dom = new JSDOM(response.body);
+    const holdersText = dom.window.document.querySelector(
+      "#ContentPlaceHolder1_tr_tokenHolders .mr-3"
+    ).textContent;
 
+    const holders = parseInt(
+      holdersText.replace(" addresses", "").replace(/,/g, ""),
+      10
+    );
 
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    console.log("Holders: ", holders);
+    await saveData("cars", holders, databaseSDK);
+
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const tokens = [
@@ -60,12 +61,13 @@ const tokens = [
 ];
 
 const main = async () => {
+  const databaseSDK = admin.database();
   for (const token of tokens) {
-    await getHolders(token);
+    await getHolders(token, databaseSDK);
   }
-  console.log(3, 'pasaaa')
-  
+
+  // setTimeout( () => main(), 15000);
 };
 
-main().then(console.log('a'));
-console.log(4, 'pasaaa')
+main()
+
